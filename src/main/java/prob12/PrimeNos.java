dec2906a584c333b6d6027320f7c72b872abb480
Joy Mendonca <joymendonca1999@gmail.com>
2020-02-11 00:52:06 +0530

    Practice Probs

---
+++ src/main/java/prob12/PrimeNos.java
@@ -0,0 +1,16 @@
+package prob12;
+
+public class PrimeNos {
+    public static void main(String[] args) {
+        for(int i=3;i<=100;i++){
+            int state = 0;
+            for(int j=2;j<i;j++){
+                if(i%j == 0)
+                    state++;
+            }
+            if(state == 0){
+                System.out.print(i+", ");
+            }
+        }
+    }
+}
